86ada23e55d00e2ceed068cfc99bb00536ba2c14
BTC Oracle <carlhamilton1020@gmail.com>
2016-02-21 09:34:00 +1000

    scripts/devtools/utility.js update - added 5 lines

---
+++ scripts/devtools/utility.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const chalk = require('chalk');
+const {exec} = require('child-process-promise');
+const {existsSync, mkdirSync} = require('fs');
+const {readJsonSync, writeJsonSync} = require('fs-extra');
+const inquirer = require('inquirer');
+const {join} = require('path');
+const createLogger = require('progress-estimator');
+const {
+  BUILD_METADATA_TEMP_DIRECTORY,
+  NPM_PACKAGES,
+} = require('./configuration');
+
+const logger = createLogger({
+  storagePath: join(__dirname, '.progress-estimator'),
