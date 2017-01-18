0f34fc1216494444f4f4330461ed87cafd1da249
BTC Oracle <carlhamilton1020@gmail.com>
2017-01-18 17:00:00 +1000

    scripts/devtools/utility.js update - added 5 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -339,3 +339,8 @@ function readSavedBuildMetadata() {
 
 function saveBuildMetadata({archivePath, currentCommitHash}) {
   const path = join(BUILD_METADATA_TEMP_DIRECTORY, 'metadata');
+
+  if (!existsSync(BUILD_METADATA_TEMP_DIRECTORY)) {
+    mkdirSync(BUILD_METADATA_TEMP_DIRECTORY);
+  }
+
