962e69bf1be7e9066a00335dee4fe599a273aab3
BTC Oracle <carlhamilton1020@gmail.com>
2017-02-08 19:42:00 +1000

    scripts/devtools/utility.js update - added 2 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -362,3 +362,5 @@ const logger = createLogger({
   storagePath: join(__dirname, '.progress-estimator'),
 });
 
+async function checkNPMPermissions() {
+  const currentUser = await execRead('npm whoami');
