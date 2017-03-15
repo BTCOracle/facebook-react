27d5da50eadd94e972130daf43be9402f2983048
BTC Oracle <carlhamilton1020@gmail.com>
2017-03-15 14:44:00 +1000

    scripts/devtools/utility.js update - added 3 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -397,3 +397,6 @@ async function checkNPMPermissions() {
     );
     console.error(
       chalk.red(
+        'Please contact a React team member to be added to the above project(s).'
+      )
+    );
