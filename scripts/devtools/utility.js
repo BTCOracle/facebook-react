52eb7341f5af07bcf0ab3bb09dc06352728d0c7b
BTC Oracle <carlhamilton1020@gmail.com>
2017-05-04 15:02:00 +1000

    scripts/devtools/utility.js update - added 4 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -442,3 +442,13 @@ function readSavedBuildMetadata() {
   const path = join(BUILD_METADATA_TEMP_DIRECTORY, 'metadata');
 
   if (!existsSync(path)) {
+    console.error(chalk.red('Expected to find build metadata at:'));
+    console.error(chalk.dim(`  ${path}`));
+    process.exit(1);
+  }
+
+  const {archivePath, currentCommitHash} = readJsonSync(path);
+
+  return {archivePath, currentCommitHash};
+}
+
