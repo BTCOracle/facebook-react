c7c6834dc99c37ca771320dbf14a50c0177e7f66
BTC Oracle <carlhamilton1020@gmail.com>
2017-08-14 18:10:00 +1000

    scripts/devtools/utility.js update - added 2 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -547,3 +547,5 @@ async function confirmContinue(exitFunction) {
   await confirm('Continue the release?', exitFunction);
 }
 
+async function execRead(command, options) {
+  const {stdout} = await exec(command, options);
