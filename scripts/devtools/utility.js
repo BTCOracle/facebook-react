3138794d65bd834bcf296d6049aa26accc29ba46
BTC Oracle <carlhamilton1020@gmail.com>
2017-12-26 19:23:00 +1000

    scripts/devtools/utility.js update - added 5 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -669,3 +669,10 @@ async function execRead(command, options) {
 }
 
 function readSavedBuildMetadata() {
+  const path = join(BUILD_METADATA_TEMP_DIRECTORY, 'metadata');
+
+  if (!existsSync(path)) {
+    console.error(chalk.red('Expected to find build metadata at:'));
+    console.error(chalk.dim(`  ${path}`));
+    process.exit(1);
+  }
