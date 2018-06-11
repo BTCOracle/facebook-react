340710c1cf2f51d3ea126399b3c0e315d9988d97
BTC Oracle <carlhamilton1020@gmail.com>
2018-06-11 13:20:00 +1000

    scripts/devtools/utility.js update - added 3 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -852,3 +852,6 @@ async function checkNPMPermissions() {
       chalk.red(
         `NPM user {underline ${currentUser}} is not an owner for: ${chalk.bold(
           failedProjects.join(', ')
+        )}`
+      )
+    );
