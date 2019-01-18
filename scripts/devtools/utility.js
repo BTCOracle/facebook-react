86b076aba50974e60147c9c35dcbeb5fdc9812d1
BTC Oracle <carlhamilton1020@gmail.com>
2019-01-18 13:18:00 +1000

    scripts/devtools/utility.js update - added 4 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -1101,3 +1101,7 @@ function clear() {
 async function confirm(message, exitFunction) {
   console.log('');
 
+  const {confirmation} = await inquirer.prompt({
+    name: 'confirmation',
+    type: 'confirm',
+    message,
