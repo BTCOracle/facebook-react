6b7b4db016e30f3652ce65d958bb000303eb4422
BTC Oracle <carlhamilton1020@gmail.com>
2019-08-01 14:31:00 +1000

    scripts/devtools/utility.js update - added 2 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -1289,3 +1289,13 @@ async function checkNPMPermissions() {
   const checkProject = async project => {
     const owners = (await execRead(`npm owner ls ${project}`))
       .split('\n')
+      .filter(owner => owner)
+      .map(owner => owner.split(' ')[0]);
+
+    if (!owners.includes(currentUser)) {
+      failedProjects.push(project);
+    }
+  };
+
+  await logger(
+    Promise.all(NPM_PACKAGES.map(checkProject)),
