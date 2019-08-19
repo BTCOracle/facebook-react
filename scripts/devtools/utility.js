0ebada923f3962897744af2ff4c6addea1aba0e6
BTC Oracle <carlhamilton1020@gmail.com>
2019-08-20 09:04:00 +1000

    scripts/devtools/utility.js update - added 5 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -1318,3 +1318,8 @@ async function checkNPMPermissions() {
     console.error(
       chalk.red(
         'Please contact a React team member to be added to the above project(s).'
+      )
+    );
+    process.exit(1);
+  }
+}
