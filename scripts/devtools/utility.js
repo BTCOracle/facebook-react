e72f3017e372515570a6d7e723800186cbc7c0e9
BTC Oracle <carlhamilton1020@gmail.com>
2019-09-28 20:49:00 +1000

    scripts/devtools/utility.js update - added 3 lines

--- scripts/devtools/utility.js
+++ scripts/devtools/utility.js
@@ -1362,3 +1362,6 @@ function readSavedBuildMetadata() {
   const path = join(BUILD_METADATA_TEMP_DIRECTORY, 'metadata');
 
   if (!existsSync(path)) {
+    console.error(chalk.red('Expected to find build metadata at:'));
+    console.error(chalk.dim(`  ${path}`));
+    process.exit(1);
